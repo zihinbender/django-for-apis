a084ede71e4a166a75844bd462bd3e38cf11cef2
zihinbender <73230187+zihinbender@users.noreply.github.com>
2025-12-13 20:58:33 +0000

    Blog API setup

---
+++ 03-blog/accounts/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+class CustomUserCreationForm(UserChangeForm):
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ("name",)
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm):
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
